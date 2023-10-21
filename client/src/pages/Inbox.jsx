c19d1e8f1778f7af2e3ac3eb73cf865b6ecf7145
Peter John Reyes <peter.john.reyes0326@gmail.com>
2023-10-22 02:09:54 +0800

    latest repository

--- client/src/pages/Inbox.jsx
+++ client/src/pages/Inbox.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import {useContext} from "react";
 // import {UserContext} from '../../context/userContext'
 import Sidebar from '../components/Sidebar'
 import '../css/inbox.css'
@@ -8,55 +8,57 @@ import { Link, useParams,useNavigate } from 'react-router-dom'
 import profIcon from '../assets/profile-icon.png'
 import {toast} from 'react-hot-toast'
 
+
+
 //Maglagay ng websocket para autoupdate
 
 const Inbox = () => {
-  const userId = window.localStorage.getItem("id");
-  const [tasklist, setTasklist] = useState([]);
+  const userId = window.localStorage.getItem("id")
+  const [tasklist, setTasklist] = useState([])
   useEffect(() => {
-    axios
-      .get("http://localhost:3333/tasklist/user-tasks/" + userId)
-      .then((userTasks) => {
-        setTasklist(userTasks.data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+      axios
+        .get('http://localhost:3333/tasklist/user-tasks/'+userId)
+        .then(userTasks => {
+          setTasklist(userTasks.data)
+        })
+        .catch(err => console.log(err))
+  },[])
 
-  const [username, setUsername] = useState([]);
+  const [username, setUsername] = useState([])
 
   useEffect(() => {
-    axios
-      .get("http://localhost:3333/auth/username/" + userId)
-      .then((user) => {
-        setUsername(user.data);
-      })
-      .catch((err) => console.log(err));
-  }, []);
+      axios
+        .get('http://localhost:3333/auth/username/'+userId)
+        .then(user => {
+          setUsername(user.data)
+        })
+        .catch(err => console.log(err))
+  }, [])
 
   const deleteTask = (id) => {
-    toast("Deleted Task", {
-      icon: "🗑️",
+    toast('Deleted Task', {
+      icon: '🗑️',
     });
     axios
-      .delete("http://localhost:3333/tasklist/delete/" + id)
-      .then((result) => {
+      .delete('http://localhost:3333/tasklist/delete/'+id)
+      .then(result => {
         setTimeout(() => {
           location.reload();
-        }, 1000);
+        }, 1000)
       })
-      .catch((err) => console.log(err));
-  };
+      .catch(err => console.log(err))
+  }
 
   const taskDone = (id) => {
-    toast("Congratulations on finishing this task", {
-      icon: "🥳",
+    toast('Congratulations on finishing this task', {
+      icon: '🥳',
     });
     axios
-      .delete("http://localhost:3333/tasklist/delete/" + id)
-      .then((result) => {
+      .delete('http://localhost:3333/tasklist/delete/'+id)
+      .then(result => {
         setTimeout(() => {
           location.reload();
-        }, 1000);
+        }, 1000)
       })
       .catch(err => console.log(err))
   }
@@ -64,19 +66,32 @@ const Inbox = () => {
 
   return (
     <div className="inbox-page-container">
-      <Sidebar userimg={profIcon} username={username.username} />
+    <Sidebar 
+    userimg = {profIcon}
+    username = {username.username}
+    />
 
-      <main className="inbox-page">
-        <div className="inbox-heading">
-          <i class="fa-solid fa-inbox inbox-heading-icon"></i>
-          <h1>Inbox</h1>
-        </div>
-        <div className="sort-by justify-content-end">
-          <i class="fa-solid fa-sliders sort-by-icon"></i>
-          <h5>Sort By</h5>
-        </div>
+    <main className="inbox-page">
+      <div className="inbox-heading">
+        <i class="fa-solid fa-inbox inbox-heading-icon"></i>
+        <h1>Inbox</h1>
+      </div>
 
-        {tasklist.map((task) => (
+
+      {tasklist.map(task => (
+        <div>
+        <ul 
+        className="inbox-list" 
+        style={{display: 'flex', gap: '20px'}}
+        id="myList"
+        >
+          <li 
+          onClick={() => taskDone(task._id)}
+          style={{cursor: 'pointer'}}
+          >
+            <i class="fa-regular fa-circle"></i>
+          </li>
+
           <div>
           <li><strong>{task.task_title}</strong></li>
           <li>{task.task_description}</li>
@@ -86,10 +101,29 @@ const Inbox = () => {
           </div>
           
           </div>
-        ))}
-      </main>
-    </div>
-  );
-};
 
-export default Inbox;
+        
+          <li>
+            <i class="fa-regular fa-pen-to-square"></i>
+          </li>
+
+          <li onClick={() => deleteTask(task._id)}>
+            <i class="fa-regular fa-trash-can"></i>
+          </li>
+        </ul>
+            <hr />
+            </div>
+      ))}
+
+      
+      
+    
+      
+
+    </main>
+
+    </div>
+  )
+}
+
+export default Inbox
